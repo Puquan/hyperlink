08acf2fd9222f3e6d8fb99571d4430ae0865eb8b
puquan <puquan@ualberta.ca>
2021-01-29 15:35:43 +0800

    upload by upload.py

--- generate.py
+++ generate.py
@@ -1,6 +1,6 @@
 import os
 from pyperclip import copy
-
+import git
 from urllib.parse import unquote
 from urllib.parse import quote
 def search_dir_path(git_repo_link):
@@ -11,11 +11,60 @@ def search_dir_path(git_repo_link):
             break
     return git_repo_link[index:] + '/'
 
-def generate_link(git_repo_link, notebook_path):
+def generate_link1(notebook_path):
+
+    # 当前文件夹名字
+    repo_dir = os.getcwd()
+    repo_name = repo_dir.split('/')[-1]
+    r = git.Repo(repo_dir)
 
     # hub地址变了请修改这
-    jupyterhub_URL = "http://139.217.176.104:30002/"
+    #jupyterhub_URL = "http://139.217.176.104:30002/"
+    jupyterhub_URL = 'http://218.66.48.241:8001/'
+    # 后缀1
+    following1 = 'hub/user-redirect/git-pull?repo='
 
+
+    # repo地址
+    reader = r.config_reader()
+    username = reader.get_value("user","name")
+    repo_name = repo_dir.split('/')[-1]
+    git_repo_link = 'http://139.217.176.104:31000/' + username + '/' + repo_name
+    encoded_gitrepo_url = quote(git_repo_link,'utf-8')
+
+
+    # 后缀2
+    following2 = '&urlpath=tree'
+
+    repopath = search_dir_path(git_repo_link)
+
+    # 文件夹地址
+    encoded_repopath_url = quote(repopath,'utf-8')
+
+    # notebook地址
+    encoded_notebook_url = quote(notebook_path,'utf-8')
+
+    # 后缀3，branch变了修改这
+    following3 = '&branch=master'
+
+
+    url = jupyterhub_URL + following1 + encoded_gitrepo_url + following2 + encoded_repopath_url + encoded_notebook_url + following3
+
+    return url
+def search_notebook():
+    dirs = os.listdir(os.getcwd())
+    notebook = []
+    for filename in dirs:
+        if '.ipynb' in filename and filename!='.ipynb_checkpoints':
+            notebook.append(filename)
+
+    return notebook
+
+def generate_link2(git_repo_link, notebook_path):
+
+    # hub地址变了请修改这
+    #jupyterhub_URL = "http://139.217.176.104:30002/"
+    jupyterhub_URL = 'http://218.66.48.241:8001/'
     # 后缀1
     following1 = 'hub/user-redirect/git-pull?repo='
 
--- task.py
+++ task.py
@@ -4,19 +4,44 @@ import git
 import sys
 from urllib.parse import unquote
 from urllib.parse import quote
-from generate import generate_link
+from generate import generate_link1
+from generate import generate_link2
 from init import init
 from upload import upload
-
+from generate import search_notebook
 
 def main():
-    prompt = int(input('输入模式：\n 1. 将此文件夹新建成git仓库\n 2. 更新git仓库内容\n 3. 生成git仓库notebook链接\n: ' ))
+    prompt = int(input('输入模式：\n 1. 将此文件夹新建成git仓库并上传\n 2. 更新git仓库内容\n 3. 生成自己git仓库notebook链接\n 4. 生成他人git仓库notebook链接\n: ' ))
     if prompt==1:
         init()
+        print('初始化仓库并上传文件完成')
     elif prompt==2:
         upload()
+        print('上传文件已完成')
     elif prompt==3:
-        pass
+        notebook = search_notebook()
+        print(notebook)
+        # 如果文件夹中只有一个notebook，自动生成该notebook链接
+        if len(notebook)==1:
+            link = generate_link(notebook[0])
+            print('notebook链接为：')
+            print(link)
+        elif len(notebook)>1:
+            print('检测到多个notebook，请选择一个')
+            for index in range(len(notebook)):
+                option = index + 1
+                print(str(option) + '. ' + str(notebook[index]))
+            user_choice = int(input('请输入序号: '))
+            link = generate_link1(notebook[user_choice-1])
+            print('notebook链接为：')
+            print(link)
+        else:
+            print('无法识别指令，关闭程序。')
+    elif prompt==4:
+        git_repo_link = input('请输入该git仓库网址：')
+        notebook_path = input('请输入notebook在该仓库的相对地址：')
+        link = generate_link2(git_repo_link,notebook_path)
+        print(link)
     else:
         print('无法识别指令，关闭程序。')
 main()
